f079cf606ee5233bee4de61f998e6a4bb383d696
TimmyMill <zp9825it@go.minneapolis.edu>
2015-09-30 16:17:08 -0500

    Added name and color to Dinosaur class.

---
+++ Dinosaur.java
@@ -0,0 +1,14 @@
+package com.timmy;
+
+/**
+ * Created by smoke on 9/30/15.
+ */
+public class Dinosaur {
+
+    String name;
+    String color;
+
+    public void roar() {
+        System.out.println("GRRR!!!!!");
+    }
+}
---
+++ DinosaurSupervisor.java
@@ -0,0 +1,22 @@
+package com.timmy;
+
+public class DinosaurSupervisor {
+
+    public static void main(String[] args) {
+
+        // Create a dinosaur object. Invokes Dinosaur constructor.
+        Dinosaur t_rex = new Dinosaur();
+        t_rex.color = "red";
+        t_rex.name = "Tyrannosaurus Rex";
+        t_rex.roar();
+
+        System.out.println("The name of this dinosaur is " + t_rex.name);
+        System.out.println("Its color is " + t_rex.color);
+
+        Dinosaur velociraptor = new Dinosaur();
+        velociraptor.color = "blue";
+        velociraptor.name = "Velociraptor";
+        velociraptor.roar();
+
+    }
+}
